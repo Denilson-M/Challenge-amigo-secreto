025c14f46a79d4b3afd4ca97f604dd5db9513a49
Denilson <denilsonmoraes10000@gmail.com>
2025-01-20 21:00:04 -0300

    Projeto Inicial

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+
+function adicionarAmigo()
+{
+    let nome = document.querySelector("input").value;
+
+    if(nome == "")
+    {
+        alert("Por favor, insira um nome.");
+    }
+    else
+    {
+        amigos.push(amigos);
+        limparCampo();
+    }
+}
+
+function sortearAmigo()
+{
+    alert("O botao Sortear foi clicado");
+}
+
+function limparCampo()
+{
+    nome = document.querySelector("input");
+    nome.value = "";
+}
